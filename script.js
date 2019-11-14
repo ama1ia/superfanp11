2f274f0f893d1dd8d3af1d3f0d174f7c00e48c7e
ama1ia <45046116+ama1ia@users.noreply.github.com>
2019-11-14 12:31:44 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+function firstImage() {
+  var popup1 = document.getElementById("ET");
+  popup1.classList.toggle("show");
+}
+
+function secondImage() {
+  var popup2 = document.getElementById("LToP");
+  popup2.classList.toggle("show");
+}
+
+function thirdImage() {
+  var popup3 = document.getElementById("SoL");
+  popup3.classList.toggle("show");
+}
+
+function fourthImage() {
+  var popup4 = document.getElementById("StMB");
+  popup4.classList.toggle("show");
+}
+
+function fifthImage() {
+  var popup5 = document.getElementById("CtR");
+  popup5.classList.toggle("show");
+}
+
+function sixthImage() {
+  var popup6 = document.getElementById("AdT");
+  popup6.classList.toggle("show");
+}
+
+function seventhImage() {
+  var popup7 = document.getElementById("BhP");
+  popup7.classList.toggle("show");
+}
+
+function eighthImage() {
+  var popup8 = document.getElementById("C");
+  popup8.classList.toggle("show");
+}
+
+function ninthImage() {
+  var popup9 = document.getElementById("GGB");
+  popup9.classList.toggle("show");
+}
+
+function tenthImage() {
+  var popup10 = document.getElementById("GWoC");
+  popup10.classList.toggle("show");
+}
+
+function eleventhImage() {
+  var popup11 = document.getElementById("MP");
+  popup11.classList.toggle("show");
+}
+
+function twelfthImage() {
+  var popup12 = document.getElementById("TMh");
+  popup12.classList.toggle("show");
+}
+
